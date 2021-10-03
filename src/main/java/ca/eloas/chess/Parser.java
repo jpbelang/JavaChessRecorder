47c0cb5361fa521a19ff4a7f9ec87ace2c644410
JP Belanger <jpbelang@gmail.com>
2021-10-03 19:26:33 -0400

    Simplified regex (split)

--- src/main/java/ca/eloas/chess/Parser.java
+++ src/main/java/ca/eloas/chess/Parser.java
@@ -6,24 +6,44 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
 
-    private static final Pattern command = Pattern.compile("(\\d+)\\.(\\.\\.)? ([PRNBQK][a-h][1-8])( ([PRNBQK][a-h][1-8]))?");
+    private static final Pattern LINE = Pattern.compile("(\\d+)\\.(\\.\\.)? (\\S*)( (\\S*))?");
+    private static final Pattern COMMAND = Pattern.compile("[PRNBQK][a-h][1-8]");
 
     public List<ChessCommand> parseCommands(Reader reader) throws IOException {
 
         var line = new BufferedReader(reader);
-        var match = command.matcher(line.readLine());
+        var match = LINE.matcher(line.readLine());
         List<ChessCommand> comm = new ArrayList<>();
         if ( match.matches()) {
 
             int moveNumber = Integer.parseInt(match.group(1));
             boolean isEllipsis = "..".equals(match.group(2));
-            var whiteMove = match.group(3);
-            Optional<String> blackMove = Optional.ofNullable(match.group(5));
-            buildMove(moveNumber, isEllipsis, whiteMove.charAt(0), whiteMove.substring(1), blackMove.map(m -> m.charAt(0)).orElse('z'), blackMove.map(m -> m.substring(1)).orElse(null) );
+            var white = match.group(3);
+            var black = Optional.ofNullable(match.group(5));
+
+            var whiteMatcher = COMMAND.matcher(white);
+            if (! whiteMatcher.matches() ) {
+                throw new RuntimeException();
+            }
+
+            var blackMatcher = black.map(COMMAND::matcher);
+            if ( blackMatcher.isPresent() && !blackMatcher.get().matches() ) {
+
+                throw new RuntimeException();
+            }
+
+            var whiteMove = whiteMatcher.group();
+
+            Character blackPiece = blackMatcher.map(m -> m.group(0).charAt(0)).orElse('z');
+            String blackMove = blackMatcher.map(m -> m.group(0).substring(1)).orElse(null);
+            buildMove(moveNumber, isEllipsis,
+                    whiteMove.charAt(0), whiteMove.substring(1),
+                    blackPiece, blackMove);
             comm.add(() -> {});
             comm.add(() -> {});
         }
